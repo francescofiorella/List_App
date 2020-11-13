eefb292f4f39fc2634f1c49201955218dd6aba63
francesco_fiorella <francescofio99@gmail.com>
2020-11-13 23:18:24 +0100

    Start to define list and array, also created my_listview_detail.xml that will contain the custom layout of the list

--- app/src/main/java/com/example/listapp/MainActivity.java
+++ app/src/main/java/com/example/listapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.listapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ResourceBundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    // definizione lista e arrays
+    ListView myListView;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        // collegamento lista e arrays
+        myListView = (ListView)findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+
     }
 }
